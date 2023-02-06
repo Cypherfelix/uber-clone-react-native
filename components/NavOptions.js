157ae25bf83780f0db9f2bf792aab06562d87132
Cypherfelix <felixmarvinonline@gmail.com>
2023-02-06 15:23:06 +0300

    Building the NavOptions Component

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,14 +1,56 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { FlatList, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import tw from 'tailwind-react-native-classnames'
 import React from 'react'
+import { Icon } from 'react-native-elements';
+
+const data = [
+    {
+        id: "123",
+        title: "Get a ride",
+        image: "http://links.papareact.com/3pn",
+        screen: "MapScreen"
+    },
+    {
+        id: "456",
+        title: "Order food",
+        image: "http://links.papareact.com/28w",
+        screen: "EatsScreen"
+    },
+
+];
 
 const NavOptions = () => {
     return (
-        <View>
-            <Text>NavOptions</Text>
-        </View>
+
+        <FlatList
+            data={data}
+            horizontal
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => (
+                <TouchableOpacity
+                    style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+                    <Image
+                        style={{
+                            height: 120,
+                            width: 120,
+                            resizeMode: "contain"
+                        }}
+                        source={{
+                            uri: item.image
+                        }}
+                    />
+                    <Text style={tw`mt-2 text-lg font-semibold`} >{item.title}</Text>
+                    <Icon
+                        style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                        name='arrowright' color="white" type='antdesign' />
+                </TouchableOpacity>
+            )}
+        />
     )
 }
 
 export default NavOptions
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+
+})
